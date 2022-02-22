653354dcf538ddd63051fe83821b24bae8ee88c6
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2022-02-22 13:18:31 +0200

    Maybe better example

--- examples/indexing.py
+++ examples/indexing.py
@@ -6,6 +6,6 @@ class IndexedList():
     def __getitem__(self, i):
         ''' Map letters to integers and use them to index the list '''
         if not isinstance(i, str):
-            raise IndexError("")
+            i = str(i)
         index = ord(i)
         return self.l[index]
